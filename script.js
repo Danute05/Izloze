5e28b99af2bb452099f690cae7edfd8ef04d9a21
Danute05 <100909581+Danute05@users.noreply.github.com>
2022-03-08 10:35:42 +0200

    alvas

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const vardi=['Jana Krūmiņa','Aivars Radziņš','Jānis Bērziņš','Elīna Bileskalna','Laura Priede'];
 const balva=['Dators','Telefons','Mašīna','Televizors'];
 const naudaKopa= 12000;
+
+Math.random=Math.random()*10;
+rand=Math.floor(rand);//noapaļo uz leju
 console.log(vardi[rand]);
