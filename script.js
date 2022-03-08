4dfa0547e5fc72f89042aef8ae488ef3ef9c71e3
Danute05 <100909581+Danute05@users.noreply.github.com>
2022-03-08 11:14:33 +0200

    Stundas darbs

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const vardi=['Jana Krūmiņa','Aivars Radziņš','Jānis Bērziņš','Elīna Bileskalna','Laura Priede'];
-const balva=['Dators','Telefons','Mašīna','Televizors'];
-const naudaKopa= 12000;
+const vardi = ['Jana Krūmiņa', 'Aivars Radziņš', 'Jānis Bērziņš', 'Elina Bileskalna', 'Laura Priede'];
+const balva = ['Dators', 'Telefons', 'Mašīna', 'Televizors'];
+const naudaKopa = 12000;
 
-Math.random=Math.random()*10;
-rand=Math.floor(rand);//noapaļo uz leju
+for (let i = 0; i < uzvaretajuSkaits; i++) {
+
+    let rand = Math.random() * vardi.length;
+    rand = Math.floor(rand);//noapaļo uz leju
+
+}
+let uvaretajs = vardi[rand];
+let rindas = document.querySelector('.rindas');
+Math.random = Math.random() * 10;
 console.log(vardi[rand]);
+rindas.innerHTML += ``//pievieno+,lai pieskaitītu nākamo
+<tr>
