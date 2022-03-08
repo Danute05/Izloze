6519e90d44636fbcf9293d3e2a3ec8ba7a5668b7
Danute05 <100909581+Danute05@users.noreply.github.com>
2022-03-08 10:18:25 +0200

    Balva

---
+++ script.js
@@ -0,0 +1,4 @@
+const vardi=['Jana Krūmiņa','Aivars Radziņš','Jānis Bērziņš','Elīna Bileskalna','Laura Priede'];
+const balva=['Dators','Telefons','Mašīna','Televizors'];
+const naudaKopa= 12000;
+console.log(vardi[rand]);
