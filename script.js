691f3c5da18b4f63ea8a0fc515a1107463ef6706
Danute05 <100909581+Danute05@users.noreply.github.com>
2022-03-22 11:14:15 +0200

    Izloze

--- script.js
+++ script.js
@@ -3,20 +3,23 @@ const balva = ['Dators', 'Telefons', 'Mašīna', 'Televizors','Ledusskapis'];
 const naudaKopa = 100000;
 let uzvaretajuSkaits = 5;
 let rindas = document.querySelector('.rindas');
-rindas.innerHTML = ' ';
+rindas.innerHTML = ' ';//pievieno+,lai pieskaitītu nākamo
 for (let i = 0; i < uzvaretajuSkaits; i++) {
     let rand = Math.random() * vardi.length;
     rand = Math.floor(rand);//noapaļo uz leju
     let uzvaretajs = vardi[rand];
-    rindas.innerHTML +=
+    let balvas = balva[rand];
+    
+        rindas.innerHTML +=
         <tr>
-            <td>${i + 1}</td>
+            <td>${i+1}</td>
             <td>${uzvaretajs}</td>
             <td>${laimesti}</td>
-            let uzvaretajs = vardi[rand];
         </tr>//izvada vairakas rindas
+
+        
 }
-rindas.innerHTML += ``//pievieno+,lai pieskaitītu nākamo
+
 
 
 
